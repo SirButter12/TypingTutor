22fddd1329469455e144e89bd3eb16da9e584740
SirButter12 <cianobed@gmail.com>
2026-09-17 19:39:16 -0400

    Basic text implemented

--- src/main/java/com/programacion3/typingtutor/App.java
+++ src/main/java/com/programacion3/typingtutor/App.java
@@ -1,10 +1,13 @@
 package com.programacion3.typingtutor;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.shape.Rectangle;
@@ -21,11 +24,28 @@ public class App extends Application {
         BorderPane root = new BorderPane();
         VBox top = new VBox();
         Label title = new Label("Typing tutor");
-    
+        
+        top.getChildren().add(title);
+        
         VBox center = new VBox();
         StackPane sampleTextSection = new StackPane();
         GridPane lettersLayer = new GridPane();
         GridPane markerLayer = new GridPane();
+        
+        String textToType = "asdadasdsgsadgfhsdadfadgshdgbhf";
+        
+        for (int i = 0; i < 20; i++) {
+            ColumnConstraints column = new ColumnConstraints(30, 30, 30);
+            lettersLayer.getColumnConstraints().add(column);
+        }
+        
+        for (int i = 0; i < (textToType.length() / 20); i++) {
+            RowConstraints row = new RowConstraints(30, 30, 30);
+            lettersLayer.getRowConstraints().add(row);
+        }
+        
+        
+        distributeString(textToType, lettersLayer);
         
         Rectangle marker = new Rectangle(30, 30);
         markerLayer.add(marker, 0, 0);
@@ -36,7 +56,6 @@ public class App extends Application {
         GridPane pressedKeyLayer = new GridPane();
         GridPane keyBoardLayer = new GridPane();
         
-        top.getChildren().add(title);
         
         sampleTextSection.getChildren().add(markerLayer);
         sampleTextSection.getChildren().add(lettersLayer);
@@ -45,9 +64,10 @@ public class App extends Application {
         virtualKeyBoardSection.getChildren().add(pressedKeyLayer);
         virtualKeyBoardSection.getChildren().add(keyBoardLayer);
         
-        root.setTop(title);
+        root.setTop(top);
         root.setCenter(center);
         
+        root.setPadding(new Insets(20));
         Scene scene = new Scene(root, 640, 480);
         stage.setScene(scene);
         stage.show();
@@ -57,4 +77,20 @@ public class App extends Application {
         launch();
     }
 
+    public static void distributeString(String txt, GridPane letters) {
+        int len = txt.length();
+        int row = 0;
+        int column = 0;
+        
+        for (int i = 0; i < len; i++) {
+            if (column == 20) {
+                row++;
+                column = 0;
+            }            
+            
+            letters.add(new Label(txt.substring(i, i + 1)), column, row);
+            
+            column++;
+        }
+    }    
 }
