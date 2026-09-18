162017a9f04a6655b197d5c64665ee297dccafb5
SirButter12 <cianobed@gmail.com>
2026-09-17 20:33:29 -0400

    keyboard half implemented

--- src/main/java/com/programacion3/typingtutor/App.java
+++ src/main/java/com/programacion3/typingtutor/App.java
@@ -1,5 +1,7 @@
 package com.programacion3.typingtutor;
 
+import java.util.ArrayList;
+import java.util.List;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
@@ -18,7 +20,25 @@ import javafx.stage.Stage;
  * JavaFX App
  */
 public class App extends Application {
-    public static String textToType = "asdadasdsgsadgfhsdadfadgshdgbhf";
+    public static String textToType = "";
+    
+    public static List<String> keys = new ArrayList<>(List.of(
+            "1","2","3","4","5", "6", "7", "8", "9", "0", "\n"
+            ,"tab" , "q", "w", "e", "r", "t", "y", "u", "i" , "o", "p","tab" ,"\n"
+            ,"shift", "a", "s", "d", "f", "g", "h", "j", "k" , "l", ";","shift" ,"\n"
+            ,"alt" , "z", "x", "c", "v", "b", "n", "m", "," , ".", "/","alt"
+    ));
+    
+    public static List<String> shiftKeys = new ArrayList<>(List.of(
+            "!","@","#","$","%", "^", "&", "*", "(", ")", "\n"
+            ,"tab" , "Q", "W", "E", "R", "T", "Y", "U", "I" , "O", "P","tab" ,"\n"
+            ,"shift" , "A", "S", "D", "F", "G", "H", "J", "K" , "L", ":","shift" ,"\n"
+            ,"alt" ,"Z", "X", "C", "V", "B", "N", "M", "<" , ">", "?","alt"
+    ));
+    
+    
+    
+    
     
     @Override
     public void start(Stage stage) {
@@ -38,14 +58,15 @@ public class App extends Application {
         formatLayers(lettersLayer);
         
         Rectangle marker = new Rectangle(30, 30);
-        markerLayer.add(marker, 0, 1);
+        markerLayer.add(marker, 0, 0);
        
         center.getChildren().add(sampleTextSection);
         StackPane virtualKeyBoardSection = new StackPane();
         GridPane instructionLayer = new GridPane();
         GridPane pressedKeyLayer = new GridPane();
-        GridPane keyBoardLayer = new GridPane();
+        GridPane keyBoardLayer = new GridPane();     
         
+        fillKeyboard(keys, keyBoardLayer);
         
         sampleTextSection.getChildren().add(markerLayer);
         sampleTextSection.getChildren().add(lettersLayer);
@@ -53,6 +74,7 @@ public class App extends Application {
         virtualKeyBoardSection.getChildren().add(instructionLayer);
         virtualKeyBoardSection.getChildren().add(pressedKeyLayer);
         virtualKeyBoardSection.getChildren().add(keyBoardLayer);
+        center.getChildren().add(virtualKeyBoardSection);
         
         root.setTop(top);
         root.setCenter(center);
@@ -61,10 +83,6 @@ public class App extends Application {
         Scene scene = new Scene(root, 640, 480);
         stage.setScene(scene);
         stage.show();
-    }
-
-    public static void main(String[] args) {
-        launch();
     }
 
     public static void distributeString(String txt, GridPane letters) {
@@ -86,13 +104,42 @@ public class App extends Application {
     
     public static void formatLayers(GridPane gridPane) {
         for (int i = 0; i < 20; i++) {
-            ColumnConstraints column = new ColumnConstraints(30, 30, 30);
+            ColumnConstraints column = new ColumnConstraints(30);
             gridPane.getColumnConstraints().add(column);
         }
         
         for (int i = 0; i < (textToType.length() / 20); i++) {
-            RowConstraints row = new RowConstraints(30, 30, 30);
+            RowConstraints row = new RowConstraints(30);
             gridPane.getRowConstraints().add(row);
         }
-    }    
+    }
+    
+    public static void fillKeyboard(List<String> keyBoard, GridPane keyBoardLayer) {
+        for (int i = 0; i < 12; i++) {
+            ColumnConstraints column = new ColumnConstraints(30, 30, 999);
+            keyBoardLayer.getColumnConstraints().add(column);
+        }
+        
+        for (int i = 0; i < (textToType.length() / 20); i++) {
+            RowConstraints row = new RowConstraints(30, 30, 30);
+            keyBoardLayer.getRowConstraints().add(row);
+        }
+        
+        int row = 0;
+        int column = 0;
+        for (String key: keyBoard) {
+            if (key.equals("\n")){
+                row++;
+                column = 0;
+            }
+            
+            keyBoardLayer.add(new Label(key), column, row);
+            
+            column++;
+        }
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
