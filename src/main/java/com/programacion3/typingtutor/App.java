fe06a45e66bb3ec4621b89d2140988ac95d7e897
SirButter12 <cianobed@gmail.com>
2026-09-22 18:50:24 -0400

    keyboard starting to be functional

--- src/main/java/com/programacion3/typingtutor/App.java
+++ src/main/java/com/programacion3/typingtutor/App.java
@@ -1,13 +1,16 @@
 package com.programacion3.typingtutor;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import javafx.application.Application;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.VPos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
@@ -38,9 +41,7 @@ public class App extends Application {
             ,"alt" ,"Z", "X", "C", "V", "B", "N", "M", "<" , ">", "?","alt"
     ));
     
-    
-    
-    
+    public static Map<KeyCode, int[]> keyMapping = new HashMap(); 
     
     @Override
     public void start(Stage stage) {
@@ -83,6 +84,67 @@ public class App extends Application {
         
         root.setPadding(new Insets(20));
         Scene scene = new Scene(root, 640, 480);
+        
+        
+        
+        keyMapping.put(KeyCode.DIGIT1, new int[]{0, 0});
+        keyMapping.put(KeyCode.DIGIT2, new int[]{1, 0});
+        keyMapping.put(KeyCode.DIGIT3, new int[]{2, 0});
+        keyMapping.put(KeyCode.DIGIT4, new int[]{3, 0});
+        keyMapping.put(KeyCode.DIGIT5, new int[]{4, 0});
+        keyMapping.put(KeyCode.DIGIT6, new int[]{5, 0});
+        keyMapping.put(KeyCode.DIGIT7, new int[]{6, 0});
+        keyMapping.put(KeyCode.DIGIT8, new int[]{7, 0});
+        keyMapping.put(KeyCode.DIGIT9, new int[]{8, 0});
+        keyMapping.put(KeyCode.DIGIT0, new int[]{9, 0});
+        keyMapping.put(KeyCode.TAB, new int[]{0, 1});
+        keyMapping.put(KeyCode.Q, new int[]{1, 1});
+        keyMapping.put(KeyCode.W, new int[]{2, 1});
+        keyMapping.put(KeyCode.E, new int[]{3, 1});
+        keyMapping.put(KeyCode.R, new int[]{4, 1});
+        keyMapping.put(KeyCode.T, new int[]{5, 1});
+        keyMapping.put(KeyCode.Y, new int[]{6, 1});
+        keyMapping.put(KeyCode.U, new int[]{7, 1});
+        keyMapping.put(KeyCode.I, new int[]{8, 1});
+        keyMapping.put(KeyCode.O, new int[]{9, 1});
+        keyMapping.put(KeyCode.P, new int[]{10, 1});
+        keyMapping.put(KeyCode.SHIFT, new int[]{0, 2});
+        keyMapping.put(KeyCode.A, new int[]{1, 2});
+        keyMapping.put(KeyCode.S, new int[]{2, 2});
+        keyMapping.put(KeyCode.D, new int[]{3, 2});
+        keyMapping.put(KeyCode.F, new int[]{4, 2});
+        keyMapping.put(KeyCode.G, new int[]{5, 2});
+        keyMapping.put(KeyCode.H, new int[]{6, 2});
+        keyMapping.put(KeyCode.J, new int[]{7, 2});
+        keyMapping.put(KeyCode.K, new int[]{8, 2});
+        keyMapping.put(KeyCode.L, new int[]{9, 2});
+        keyMapping.put(KeyCode.SEMICOLON, new int[]{10, 2});
+        keyMapping.put(KeyCode.ALT, new int[]{0, 3});
+        keyMapping.put(KeyCode.Z, new int[]{1, 3});
+        keyMapping.put(KeyCode.X, new int[]{2, 3});
+        keyMapping.put(KeyCode.C, new int[]{3, 3});
+        keyMapping.put(KeyCode.V, new int[]{4, 3});
+        keyMapping.put(KeyCode.B, new int[]{5, 3});
+        keyMapping.put(KeyCode.N, new int[]{6, 3});
+        keyMapping.put(KeyCode.M, new int[]{7, 3});
+        keyMapping.put(KeyCode.COMMA, new int[]{8, 3});
+        keyMapping.put(KeyCode.PERIOD, new int[]{9, 3});
+        keyMapping.put(KeyCode.SLASH, new int[]{10, 3});
+        
+        scene.setOnKeyPressed(event -> {
+            if (event.getCode() == KeyCode.SHIFT) {
+                updateKeyboard(shiftKeys, keyBoardLayer);
+            }
+        }); 
+        
+        scene.setOnKeyReleased(event -> {
+            if (event.getCode() == KeyCode.SHIFT) {
+                updateKeyboard(keys, keyBoardLayer);
+            }
+        }); 
+        
+        
+        
         stage.setScene(scene);
         stage.show();
     }
@@ -123,21 +185,13 @@ public class App extends Application {
         }
     }
     
-   public static void fillKeyboard(List<String> keyBoard, GridPane keyBoardLayer) {
-    for (int i = 0; i < 12; i++) {
-        ColumnConstraints column = new ColumnConstraints(30, 30, 999);
-        keyBoardLayer.getColumnConstraints().add(column);
-    }
-
-    for (int i = 0; i < 4; i++) {
-        RowConstraints row = new RowConstraints(30, 30, 30);
-        keyBoardLayer.getRowConstraints().add(row);
-    }
+    public static void updateKeyboard(List<String> keyboard, GridPane keyBoardLayer) {
+    keyBoardLayer.getChildren().clear();
 
     int row = 0;
     int column = 0;
 
-    for (String key : keyBoard) {
+    for (String key : keyboard) {
         if (key.equals("\n")) {
             row++;
             column = 0;
@@ -153,6 +207,20 @@ public class App extends Application {
 
         column++;
     }
+}
+    
+   public static void fillKeyboard(List<String> keyBoard, GridPane keyBoardLayer) {
+    for (int i = 0; i < 12; i++) {
+        ColumnConstraints column = new ColumnConstraints(30, 30, 999);
+        keyBoardLayer.getColumnConstraints().add(column);
+    }
+
+    for (int i = 0; i < 4; i++) {
+        RowConstraints row = new RowConstraints(30, 30, 30);
+        keyBoardLayer.getRowConstraints().add(row);
+    }
+
+    updateKeyboard(keyBoard, keyBoardLayer);
 }
 
     public static void main(String[] args) {
